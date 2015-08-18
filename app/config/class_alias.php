c52b21ca2c204c381bc2faf550a3dc434ab68c2f
Roberto Cervantes <megas.roberto@gmail.com>
2015-08-18 19:28:42 +0200

    Added class alias: Sentry, Carbon

--- app/config/class_alias.php
+++ app/config/class_alias.php
@@ -1,3 +1,5 @@
 <?php
+	class_alias('Cartalyst\Sentry\Facades\Native\Sentry', 'Sentry');
 	class_alias('Illuminate\Database\Eloquent\Model', 'Model');
 	class_alias('Illuminate\Database\Capsule\Manager', 'DB');
+	class_alias('Carbon\Carbon', 'Carbon');
